81dffbea01bbf766618c81a5ed09a4beb1f401f2
Sultan Alsawaf <sultan@kerneltoast.com>
2021-07-17 09:55:50 -0700

    mm: Eliminate d_path_outlen() and further speed up show_map_vma()
    d_path_outlen() isn't needed because we know that d_path() always
    populates the given buffer backwards starting from the last byte; with
    this, we can easily calculate the length of the generated string by
    using the returned pointer from d_path() and the size of the buffer
    given to d_path(). This eliminates the need for d_path_outlen() and
    removes the bizarre strlen() usage, which makes things simpler and
    faster. We also now avoid a memmove() when d_path() completely uses up
    its provided buffer.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -16,7 +16,7 @@
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
 
-#define LIST_WL_DEFAULT			"wlan_wake;wlan_rx_wake;wlan_ctrl_wake;wlan_txfl_wake;bluetooth_timer;BT_bt_wake;BT_host_wake;bbd_wake_lock;ssp_sensorhub_wake_lock;ssp_wake_lock;ssp_comm_wake_lock;mmc0_detect;grip_wake_lock;wlan_scan_wake;wlan_pm_wake;nfc_wake_lock"
+#define LIST_WL_DEFAULT			"wlan_wake;wlan_rx_wake;wlan_ctrl_wake;wlan_txfl_wake;bluetooth_timer;BT_bt_wake;BT_host_wake;bbd_wake_lock;ssp_sensorhub_wake_lock;ssp_wake_lock;ssp_comm_wake_lock;mmc0_detect;grip_wake_lock;wlan_scan_wake;wlan_pm_wake;nfc_wake_lock;qcom_rx_wakelock;qcom_tx_wakelock;NETLINK;IPA_WS;netmgr_wl;wlan_wow_wl;wlan_extscan_wl;PowerManagerService;wlan_ipa_wake;radio-interface;cne_wake_lock;qcom_smux_wakelock;mdss_dsi_wakelock"
 
 #define LENGTH_LIST_WL				1024
 #define LENGTH_LIST_WL_DEFAULT		(sizeof(LIST_WL_DEFAULT))
