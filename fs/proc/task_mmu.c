3d6ad51afa15d5a4d3dbdfc9ebde83f8b4d11050
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-12-03 20:52:40 +0800

    /proc/pid/maps: fake jit-zygote-cache flags + fixes
    munel: *adapt and additional fixes
    
    fix:
    jit mapping anonymization and permissions
    
    fixup for error like this:
    Fatal signal 11 (SIGSEGV), code 2 (SEGV_ACCERR),
    fault addr 0x97d83bf0 in tid Thread-27
    00 pc 0000000000008bf0 /memfd:jit-zygote-cache (deleted)
    01 pc 00000000006157b0 libart.so (nterp_helper+4016)
    06 pc 0000000000358e40 libart.so (ClassLinker::InitializeClass+4912)
    
    Signed-off-by: munel <starscroch90@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -538,22 +538,31 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 	unsigned long start, end;
 	dev_t dev = 0;
 	const char *name = NULL;
-	struct dentry *dentry;
 
 	if (file) {
 		struct inode *inode = file_inode(file);
+		struct dentry *dentry = file->f_path.dentry;
+
 		dev = inode->i_sb->s_dev;
 		ino = inode->i_ino;
 #ifdef CONFIG_NOMOUNT
 		nomount_spoof_mmap_metadata(inode, &dev, &ino);
 #endif
 		pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
-		dentry = file->f_path.dentry;
 
-		if (dentry && strstr(dentry->d_name.name, "lineage")) {
-			show_vma_header_prefix(m, vma->vm_start, vma->vm_end, flags, pgoff, dev, ino);
-			name = "/dev/ashmem (deleted)";
-			goto done;
+		if (dentry) {
+			const char *path = dentry->d_name.name;
+
+			if (strstr(path, "lineage")) {
+				show_vma_header_prefix(m, vma->vm_start, vma->vm_end, flags, pgoff, dev, ino);
+				name = "/dev/ashmem (deleted)";
+				goto done;
+			}
+
+			if (strstr(path, "jit-")) {
+				show_vma_header_prefix(m, vma->vm_start, vma->vm_end, flags, 0, 0, 0);
+				goto done;
+			}
 		}
 	}
 
