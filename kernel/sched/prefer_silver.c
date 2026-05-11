a1ac57aa78456720bfbc0103f750c4e9e8dfb981
twenty2xtone7 <imstarvingsquid@gmail.com>
2026-05-12 00:46:34 +0700

    sched: add debug logging to prefer_silver
    - Add pr_info logging to track prefer_silver initialization
    - Add logging to check_freq, check_task_util, and find_best_silver_cpu
    - Add logging in fair.c when prefer_silver is invoked
    - Helps verify if prefer_silver is actually running on the kernel

--- kernel/sched/prefer_silver.c
+++ kernel/sched/prefer_silver.c
@@ -15,9 +15,19 @@ int sysctl_heavy_task_thresh = 50;
 int sysctl_cpu_util_thresh = 85;
 int sysctl_silver_trigger_freq = 1503000;
 
+#define PREFER_SILVER_DEBUG
+#ifdef PREFER_SILVER_DEBUG
+#define ps_debug(fmt, ...) pr_info("[PREFER_SILVER] " fmt, ##__VA_ARGS__)
+#else
+#define ps_debug(fmt, ...) no_printk(fmt, ##__VA_ARGS__)
+#endif
+
 bool prefer_silver_check_freq(int cpu)
 {
 	unsigned int freq = cpufreq_quick_get(cpu);
+	ps_debug("check_freq: cpu=%d freq=%u threshold=%d ret=%d",
+		 cpu, freq, sysctl_silver_trigger_freq,
+		 freq < sysctl_silver_trigger_freq);
 	return freq < sysctl_silver_trigger_freq;
 }
 
@@ -35,11 +45,15 @@ unsigned long ps_cpu_util(int cpu)
 bool prefer_silver_check_task_util(struct task_struct *p)
 {
 	unsigned long thresh;
+	bool ret;
 
 	thresh = capacity_orig_of(task_cpu(p)) *
 		 sysctl_heavy_task_thresh / 100;
 
-return ps_task_util(p) * 100 < thresh * 107;
+	ret = ps_task_util(p) * 100 < thresh * 107;
+	ps_debug("check_task_util: pid=%d util=%lu thresh=%lu ret=%d",
+		 p->pid, ps_task_util(p), thresh, ret);
+	return ret;
 }
 
 bool prefer_silver_check_cpu_util(int cpu)
@@ -52,6 +66,9 @@ int find_best_silver_cpu(struct task_struct *p)
 {
 	int i, best_cpu = -1;
 	unsigned long min_util = ULONG_MAX;
+
+	ps_debug("find_best_silver_cpu: pid=%d prefer_silver=%d",
+		 p->pid, sysctl_prefer_silver);
 
 	for_each_cpu(i, p->cpus_ptr) {
 		unsigned long cur_util;
@@ -72,5 +89,15 @@ int find_best_silver_cpu(struct task_struct *p)
 		}
 	}
 
+	ps_debug("find_best_silver_cpu: result best_cpu=%d", best_cpu);
 	return best_cpu;
 }
+
+static int __init prefer_silver_init(void)
+{
+	pr_info("[PREFER_SILVER] init: sysctl_prefer_silver=%d heavy_task_thresh=%d cpu_util_thresh=%d trigger_freq=%d",
+		sysctl_prefer_silver, sysctl_heavy_task_thresh,
+		sysctl_cpu_util_thresh, sysctl_silver_trigger_freq);
+	return 0;
+}
+late_initcall(prefer_silver_init);
