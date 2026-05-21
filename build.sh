2dbcf2cf92e7504129f62070d0ae6316b9338cb0
twenty2xtone7 <imstarvingsquid@gmail.com>
2026-05-21 20:30:29 +0700

    migrate from KernelSU-Next into ReSukiSu

--- build.sh
+++ build.sh
@@ -449,7 +449,7 @@ mkdir -p "$TMP_DIR"
 VERIFY_TOOLCHAIN
 VERIFY_DEFCONFIG
 
-git submodule update --init "$TOP/KernelSU-Next"
+git submodule update --init "$TOP/KernelSU"
 
 if $BUILD_KERNEL_CI; then
 	export KBUILD_BUILD_USER="Clembot"
