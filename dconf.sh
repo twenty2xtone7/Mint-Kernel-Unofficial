7b59355b0de498cebc1869fa9d9b06a45bb42973
twenty2xtone7 <imstarvingsquid@gmail.com>
2026-05-21 23:52:22 +0700

    update defconfig script

--- dconf.sh
+++ dconf.sh
@@ -1,1 +1,4 @@
-bash dfco.sh&&bash dfc.sh&& cp .config arch/arm64/configs/exynos9610-a50_core_defconfig
+bash dfco.sh&&bash dfc.sh;
+cp .config arch/arm64/configs/exynos9610-a50_core_defconfig
+git add arch/arm64/configs/exynos9610-a50_core_defconfig
+git commit -m "defconfig: reupdate config"
