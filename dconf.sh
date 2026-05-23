fec43c30b7f0abb4a260a1b3857eab5fc0708c06
twenty2xtone7 <imstarvingsquid@gmail.com>
2026-05-24 00:37:31 +0700

    update defconfig script

--- dconf.sh
+++ dconf.sh
@@ -1,4 +1,18 @@
 bash dfco.sh&&bash dfc.sh;
 cp .config arch/arm64/configs/exynos9610-a50_core_defconfig
-git add arch/arm64/configs/exynos9610-a50_core_defconfig
-git commit -m "defconfig: reupdate config"
+echo "commit changes?"
+echo "type y/n"
+read choice
+case $choice in 
+  y)
+    git add arch/arm64/configs/exynos9610-a50_core_defconfig    
+    git commit -m "defconfig: reupdate config"
+    echo "[ * ] changes commited"
+    ;;
+  n)
+    echo "[ ! ] changes not commited"
+    exit 0
+    ;;
+  *)
+    echo "[ ? ] invalid choice"
+esac
