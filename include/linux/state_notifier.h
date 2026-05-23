8e35893a8b32ae18e38c32079819ae5d3169fad5
twenty2xtone7 <imstarvingsquid@gmail.com>
2026-05-23 23:04:07 +0700

    fix ld.lld linker error

--- include/linux/state_notifier.h
+++ include/linux/state_notifier.h
@@ -10,12 +10,19 @@
 struct state_event {
 	void *data;
 };
-
+#ifdef CONFIG_STATE_NOTIFIER
 extern bool state_suspended;
 extern void state_suspend(void);
 extern void state_resume(void);
 int state_register_client(struct notifier_block *nb);
 int state_unregister_client(struct notifier_block *nb);
 int state_notifier_call_chain(unsigned long val, void *v);
-
+#else
+static const bool state_suspended = false;
+static inline void state_suspend(void) {}
+static inline void state_resume(void) {}
+static inline int state_register_client(struct notifier_block *nb) { return 0; }
+static inline int state_unregister_client(struct notifier_block *nb) { return 0; }
+static inline int state_notifier_call_chain(unsigned long val, void *v) { return 0; }
+#endif
 #endif /* _LINUX_STATE_NOTIFIER_H */
