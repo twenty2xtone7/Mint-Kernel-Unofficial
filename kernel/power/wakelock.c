85d43a53ee0ca1ba0fb6fa454ae89864420ff19c
twenty2xtone7 <imstarvingsquid@gmail.com>
2026-05-23 13:08:21 +0700

    improve deepsleep

--- kernel/power/wakelock.c
+++ kernel/power/wakelock.c
@@ -82,8 +82,8 @@ static inline void decrement_wakelocks_number(void) {}
 #endif /* CONFIG_PM_WAKELOCKS_LIMIT */
 
 #ifdef CONFIG_PM_WAKELOCKS_GC
-#define WL_GC_COUNT_MAX	100
-#define WL_GC_TIME_SEC	120
+#define WL_GC_COUNT_MAX	20
+#define WL_GC_TIME_SEC	60
 
 static void __wakelocks_gc(struct work_struct *work);
 static LIST_HEAD(wakelocks_lru_list);
