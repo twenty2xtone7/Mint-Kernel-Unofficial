8a34eb0b22b500a3f22acd292178a623f060e9c7
twenty2xtone7 <imstarvingsquid@gmail.com>
2026-05-25 14:59:52 +0700

    sched/tune: Hook task_blocklist into boost write handlers
    Prevent blocklisted processes (perfd, power-servic, etc.) from
    overwriting kernel-set cgroup boost values for app launches.

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -1,6 +1,7 @@
 #include <linux/cgroup.h>
 #include <linux/err.h>
 #include <linux/kernel.h>
+#include <linux/task_blocklist.h>
 #include <linux/percpu.h>
 #include <linux/printk.h>
 #include <linux/rcupdate.h>
@@ -535,6 +536,9 @@ boost_write(struct cgroup_subsys_state *css, struct cftype *cft,
 {
 	struct schedtune *st = css_st(css);
 
+	if (task_is_blocklisted(current))
+		return -EACCES;
+
 	if (boost < 0 || boost > 100)
 		return -EINVAL;
 
@@ -565,6 +569,9 @@ heavy_boost_write(struct cgroup_subsys_state *css, struct cftype *cft,
 {
 	struct schedtune *st = css_st(css);
 
+	if (task_is_blocklisted(current))
+		return -EACCES;
+
 	if (boost < 0 || boost > 100)
 		return -EINVAL;
 
@@ -589,6 +596,9 @@ busy_boost_write(struct cgroup_subsys_state *css, struct cftype *cft,
 	    s64 boost)
 {
 	struct schedtune *st = css_st(css);
+
+	if (task_is_blocklisted(current))
+		return -EACCES;
 
 	if (boost < 0 || boost > 100)
 		return -EINVAL;
